71f9bf5d5bf49cd5980cdffa818e5fc55a909c8c
kshitij3222 <mahajankshitij107@gmail.com>
2026-07-29 17:51:31 +0530

    Update CORS for production

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,8 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://YOUR-VERCEL-APP.vercel.app",
+      "https://youtube-final-chi.vercel.app/""
+      ,
     ],
     credentials: true,
   })
