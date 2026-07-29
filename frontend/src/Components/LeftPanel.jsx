9dbe9fea37aede7783abccecc15a48c2b22b4f2b
kshitij3222 <mahajankshitij107@gmail.com>
2026-07-29 18:54:13 +0530

    name updated

--- frontend/src/Components/LeftPanel.jsx
+++ frontend/src/Components/LeftPanel.jsx
@@ -518,7 +518,7 @@ function LeftPanel() {
           <hr className={theme ? "seperate" : "seperate-light"} />
           <Tooltip
             TransitionComponent={Zoom}
-            title="Made with 💖 by Shubhojeet"
+            title="Made with 💖 by Kshitij"
             placement="bottom"
           >
             <div className="developer">
@@ -527,11 +527,11 @@ function LeftPanel() {
                 style={{ color: theme ? "white" : "black" }}
               />
               <a
-                href="https://github.com/shubho0908"
+                href= "https://github.com/kshitij3222/youtube-final"
                 target="_blank"
                 rel="noreferrer"
               >
-                Shubhojeet Bera 🚀
+                Kshitij 🚀
               </a>
             </div>
           </Tooltip>
